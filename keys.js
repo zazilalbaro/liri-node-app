19df9b81636753bb0ffbd4b9b26ea23d0c208c3a
zazil <zaziltoma@gmail.com>
2017-10-14 00:51:06 -0700

    reformatted into functions and case stmt

--- keys.js
+++ keys.js
@@ -1,15 +1,19 @@
 // console.log('this is loaded');
 
-exports.twitterKeys = {
+var twitterKeys = {
 	consumer_key: 'JyNJYNgKeq0Fuvab1QFmnwxp9',
 	consumer_secret: 'Ttampcaht03E4q8OroEdGzrj1TLPgzWJpD9QwfiDySxt8KtuJ2',
 	access_token_key: '914720482187993088-Fa3VVrqfc0PIYaajLDnoVseJMxzunrc',
 	access_token_secret: '1jcWZv3AxJoV2XuhGaegPnJ04CBQisjQeSmWiok48X78j',
 };
 
-exports.spotifyKeys = {
+var spotifyKeys = {
 	clientID: 'f1cb81e2bcc9496ea67e06cf9bd5cc77',
 	clientSecret: 'a493ccccea6a46d480c94fc18a15e78d'
-}
+};
 
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotifyKeys: spotifyKeys
+};
 // exports.OMDBKey = '40e9cece'
--- liri.js
+++ liri.js
@@ -1,40 +1,44 @@
 var keys = require("./keys.js");
 var fs = require("fs");
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var request = require("request");
  
-
-
 var option = process.argv[2];
+var value = process.argv[3];
 
-if (option == 'my-tweets') {
+var myTweets = function() {
 	// console.log last 20 tweets & when they were created
-	var params = {screen_name: 'nodejs'};
+	var client = new Twitter(keys.twitterKeys)
+	var params = {count: 20};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  	if (!error) {
-    	console.log(tweets);
-  	}
-});
+	  	if (!error) {
+	    	for (var i = 0; i < tweets.length; i++) {
+	        	console.log(tweets[i].text + "\nTweeted on: " + tweets[i].created_at);
+	            fs.appendFile('log.txt', "\n" + tweets[i].text + "\n" + "Tweeted on: " + tweets[i].created_at + "\n", function(err) {
+					if (err) {
+						return console.log(err);
+					}
+	            });
+		  	}
+	  	}
+	});
+};
 
-}
-else if (option == 'spotify-this-song') {
-	var song = process.argv[3];
-
-	if (!song) {
-	song = 'The Sign'
+var spotifyThisSong = function(value) {
+	if (!value) {
+		value = 'The Sign'
 	}
 
 	// list artist, song name, preview link from spotify, album	
-}
-else if (option == 'movie-this') {
-	var movie = process.argv[3];
+};
 
-	if (!movie) {
-		movie = 'Mr. Nobody'
+var movieThis = function(value) {
+	if (!value) {
+		value = 'Mr. Nobody'
 	}
 
 	// connect to omdb and request data for movie
-	request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+	request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 	  // If the request is successful (i.e. if the response status code is 200)
 	  if (!error && response.statusCode === 200) {
 	  	console.log("\nTitle: " + JSON.parse(body).Title);
@@ -50,15 +54,36 @@ else if (option == 'movie-this') {
 	  	console.log("Oops, couldn't find that movie :(");
 	  };
 	});
-}
-else if (option == 'do-what-it-says') {
+};
+
+var doWhatItSays = function() {
+
 	// use the fs node package to take the text inside the random.txt and the command indicated in the file
 	// add more commands and text
-}
-else {
-	console.log("\nPlease enter one of the following:");
-	console.log("my-tweets");
-	console.log("spotify-this-song <enter song title>");
-	console.log("movie-this <enter movie title>");
-	console.log("do-what-it-says");
+};
+
+switch (option) {
+	case "my-tweets":
+		myTweets();
+		break;
+
+	case "spotify-this-song":
+		spotifyThisSong(value);
+		break;
+
+	case "movie-this":
+		movieThis(value);
+		break;
+
+	case "do-what-it-says":
+		doWhatItSays();
+		break;
+
+	default:
+		console.log("\nPlease enter one of the following:");
+		console.log("my-tweets");
+		console.log("spotify-this-song <enter song title>");
+		console.log("movie-this <enter movie title>");
+		console.log("do-what-it-says");
+		break;
 };
